7b14c4f80e33a15d85dc4a9ac22527571ad0a82b
IrakliEsephidze <irakliesephidze@gmail.com>
2025-04-28 17:48:17 +0400

    Delete quest40.js

--- quest40.js
+++
@@ -1,9 +0,0 @@
-function handleAlert() {
-    console.log('Alert triggered');
-  }
-  
-  const buttons = document.querySelectorAll('.alert-btn');
-  
-  buttons.forEach(button => {
-    button.addEventListener('click', handleAlert);
-  });
