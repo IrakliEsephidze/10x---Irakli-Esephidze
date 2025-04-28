3a5815aca60e6382f64375ae673c33ea911d415a
IrakliEsephidze <irakliesephidze@gmail.com>
2025-04-28 18:04:04 +0400

    questH10.js

--- Exam 10x/questH10.js
+++ Exam 10x/questH10.js
@@ -4,7 +4,7 @@ function createLogger(threshold = 15) {
   function logValues() {
     for (let value of values) {
       if (value > threshold) {
-        console.log(`Warning: ${value}`);
+        console.log(`Warning: ` + value);
       }
     }
   }
